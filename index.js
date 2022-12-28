9a99c2af86653da26f666d69f385f942c2f157fc
Meryem <pinarbasi.hm@gmail.com>
2022-12-28 12:07:40 +0100

    completed simple function logic

---
+++ index.js
@@ -0,0 +1,21 @@
+let input = "jAvAscript ist einfach toll";
+let output = "";
+
+
+for(let i = 0; i < input.length; i++){
+    
+    if(input[i] === "a") {
+        output += 4;
+    }else if(input[i] === "e"){
+        output += 3;
+    } else if(input[i] === "i"){
+        output += 1;
+    } else if(input[i] === "o"){
+        output += 0;
+    } else {
+        output += input[i];
+    }
+
+}
+
+console.log(output)
