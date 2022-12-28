ac5f63f07437c5668fd911c8b2eafd7b4a0c4927
Meryem <pinarbasi.hm@gmail.com>
2022-12-28 13:37:30 +0100

    completed index.js

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-let input = "jAvAscript ist einfach toll";
+let input = "javascript ist einfach toll";
 let output = "";
 
+// elements 
+let inputElement = document.getElementById('input');
+let outputElement = document.getElementById('output');
+let btnElement = document.getElementById('btn');
 
-for(let i = 0; i < input.length; i++){
+inputElement.innerHTML = input;
+
+let translateFunc = function() {
+    output = ""; 
+    input = inputElement.value;
+    input.toLowerCase();
     
-    if(input[i] === "a") {
-        output += 4;
-    }else if(input[i] === "e"){
-        output += 3;
-    } else if(input[i] === "i"){
-        output += 1;
-    } else if(input[i] === "o"){
-        output += 0;
-    } else {
-        output += input[i];
+    console.log(input)
+
+    for(let i = 0; i < input.length; i++){
+        
+        if(input[i] === "a") {
+            output += 4;
+        }else if(input[i] === "e"){
+            output += 3;
+        } else if(input[i] === "i"){
+            output += 1;
+        } else if(input[i] === "o"){
+            output += 0;
+        } else {
+            output += input[i];
+        }
     }
-
+    outputElement.innerHTML = output;
 }
-
-console.log(output)
+btnElement.addEventListener('click', translateFunc)
